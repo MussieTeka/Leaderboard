cd62d743a1a679213c96098bbd1df41f1dc2b7ed
MussieTeka <93384473+MussieTeka@users.noreply.github.com>
2023-04-12 22:40:29 +0300

    Add updated script.js file.

--- src/script.js
+++ src/script.js
@@ -2,20 +2,22 @@ import './style.css';
 
 const getScoresButton = document.querySelector('.column-top button');
 const submitForm = document.querySelector('form');
+const tableBody = document.querySelector('tbody');
 
-const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
-const gameId = '5Wo68C2OZONvrxYYkjF7';
+const baseURL =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+const gameId = 'JNpem9Nk2cN6NkOU8kz3';
 
 // Get scores for the game with the specified ID
-async function getScores(gameId) {
+const getScores = async (gameId) => {
   const response = await fetch(`${baseURL}/games/${gameId}/scores/`);
   const data = await response.json();
   const scores = data.result.sort((a, b) => b.score - a.score);
   return scores;
-}
+};
 
 // Submit a new score for the game with the specified ID
-async function submitScore(gameId, user, score) {
+const submitScore = async (gameId, user, score) => {
   const response = await fetch(`${baseURL}/games/${gameId}/scores/`, {
     method: 'POST',
     headers: {
@@ -28,24 +30,11 @@ async function submitScore(gameId, user, score) {
   });
   const data = await response.json();
   return data.result;
-}
+};
 
-// Handle form submit event to submit a new score
-submitForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const nameInput = document.querySelector('#name');
-  const scoreInput = document.querySelector('#score');
-  const name = nameInput.value;
-  const score = scoreInput.value;
-  await submitScore(gameId, name, score);
-  nameInput.value = '';
-  scoreInput.value = '';
-});
-
-// Handle "Refresh" button click to get scores for the game
-getScoresButton.addEventListener('click', async () => {
+// Display the scores in the table
+const displayScores = async () => {
   const scores = await getScores(gameId);
-  const tableBody = document.querySelector('tbody');
   tableBody.innerHTML = '';
   scores.forEach((score) => {
     const row = document.createElement('tr');
@@ -57,4 +46,25 @@ getScoresButton.addEventListener('click', async () => {
     row.appendChild(scoreCell);
     tableBody.appendChild(row);
   });
+};
+
+// Load scores when the page is first opened
+displayScores();
+
+// Handle form submit event to submit a new score
+submitForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const nameInput = document.querySelector('#name');
+  const scoreInput = document.querySelector('#score');
+  const name = nameInput.value;
+  const score = scoreInput.value;
+  await submitScore(gameId, name, score);
+  nameInput.value = '';
+  scoreInput.value = '';
+  await displayScores();
+});
+
+// Handle "Refresh" button click to get scores for the game
+getScoresButton.addEventListener('click', async () => {
+  await displayScores();
 });
